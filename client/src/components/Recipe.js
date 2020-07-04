fb91645330fb58289ac5bcea2603836df0714566
Javier Zarate <zarate.javier.jz@gmail.com>
2020-07-04 16:01:58 -0600

    added a likes state so when you click on the heart it adds a like

--- client/src/components/Recipe.js
+++ client/src/components/Recipe.js
@@ -1,18 +1,25 @@
-import React from "react"
-import { Card, CardHeader, Image, Icon } from "semantic-ui-react"
+import React, { useState, useEffect } from "react"
+import { Card, CardHeader, Image, Icon, Button } from "semantic-ui-react"
 
 const Recipe = ({ title, calories, image, time }) => {
+  const [likes, setLikes] = useState(0)
+
+  useEffect(() => {
+    console.log("effect has been ran")
+  }, [likes])
   
 const heart = (
   <a>
-    <Icon style={{ color: 'red' }} name='heart outline' />
+    <Icon onClick={() => setLikes(likes + 1)} style={{ color: 'red' }} name='heart outline' />
+
   </a>
 )
 
   return (
     <Card style={styles.containerList}>
       <Image src={image} wrapped ui={false} />
-        {heart}
+      {heart}
+      <p>{likes} likes</p>
       <CardHeader style={{ fontWeight: 'bold' }}>{title}</CardHeader>
       <Card.Meta>Calories: {Math.round(calories)}</Card.Meta>
       {/* <Card.Description>
